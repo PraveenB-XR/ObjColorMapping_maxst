1b08e4cfba48f0cfa54663368eee70ca210c1db1
Praveen B <82767364+PraveenB-XR@users.noreply.github.com>
2022-03-10 13:10:55 +0530

    Add files via upload

---
+++ Scripts/core/MaxstARColorMapping.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if USE_MAXSTAR
+using maxstAR;
+#endif
+
+public class MaxstARColorMapping : MonoBehaviour
+{
+#if USE_MAXSTAR
+    public ImageTrackableBehaviour trackableBehaviour;
+#endif
+
+    public GameObject arContents;
+
+    public int realWidth;
+    public int realHeight;
+
+    private GameObject drawObj;
+    private GameObject cube;
+
+    void Start()
+    {
+#if USE_MAXSTAR
+        float targetWidth = trackableBehaviour.TargetWidth;
+        float targetHeight = trackableBehaviour.TargetHeight;
+
+        cube = CreateCubeForMaxstARTarget(this.gameObject, targetWidth, targetHeight);
+#endif
+    }
+
+    public void Play()
+    {
+        float[] srcValue = AirarManager.Instance.CalculateMarkerImageVertex(cube);
+
+        Texture2D screenShotTex = ScreenShot.GetScreenShot(arContents);
+
+        AirarManager.Instance.ProcessColoredMapTexture(screenShotTex, srcValue, realHeight, realWidth, (resultTex) =>
+        {
+            drawObj = GameObject.FindGameObjectWithTag("coloring");
+            drawObj.GetComponent<Renderer>().material.mainTexture = resultTex;
+        });
+    }
+
+    /// <summary>
+    /// Create a full size cube on the MaxstAR marker image
+    /// </summary>
+    /// <param name="targetWidth">marker image width</param>
+    /// <param name="targetHeight">marker image height</param>
+    public GameObject CreateCubeForMaxstARTarget(GameObject parentObj, float targetWidth, float targetHeight)
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.GetComponent<Renderer>().material = AirarManager.Instance.transparentMat;
+        cube.transform.SetParent(parentObj.transform);
+        cube.transform.localPosition = Vector3.zero;
+        cube.transform.localScale = new Vector3(targetWidth, 0.001f, targetHeight);
+
+        return cube;
+    }
+}
---
+++ Scripts/core/ShotTexture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotTexture : MonoBehaviour
+{
+   
+    [SerializeField] private GameObject catGameObject;
+    [SerializeField] private Texture catTexture;
+    private Renderer catMaterial;
+
+    private void Start()
+    {
+        catMaterial = catGameObject.GetComponent<Renderer>();
+    }
+
+    public void ChangeTexture()
+    {
+        catMaterial.material.mainTexture = catTexture;
+    }
+}
+
